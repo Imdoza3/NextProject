a410242b4c2305c493fd3df80ad50d035a4d8bec
Imdoza3 <iamsteel2004@gmail.com>
2019-10-01 08:27:52 -0500

    playing with the maths and the octohorpes

--- NextProject.py
+++ NextProject.py
@@ -12,3 +12,11 @@ print("Roosters: ", 100-25* 3 % 4)
 print("Now I will count the eggs",7)
 print(3+2+1-5+4%2-1/4+6)
 print(3+2<5-7)
+
+
+
+
+
+
+# this is a print it shows whatb we wrote in the bottom of the code
+print("this is my new project")
