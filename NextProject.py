a11d2e6d06805d7b31dd3ae2a610cb0878cceaab
Imdoza3 <iamsteel2004@gmail.com>
2019-10-01 08:34:51 -0500

    describing

--- NextProject.py
+++ NextProject.py
@@ -1,9 +1,12 @@
 # this is an octohorpe -- it makes a comment (line ignored by the computer)
 # print("hello world")
+# this code code welcomes the viewers
 print("Howdy , y'all")
+# this shows what im typing
 print("i like typing this")
+# and this shows that its fun ( even though its alot of work)
 print("this is fun")
-
+# this is starting the math demo
 # math skills demo
 
 print("I will not count my chickens",2)
