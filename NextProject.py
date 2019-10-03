7b1918195a8df9b741c914135db0c45547f3442d
Imdoza3 <iamsteel2004@gmail.com>
2019-10-03 08:58:50 -0500

    My Own Variables

--- NextProject.py
+++ NextProject.py
@@ -28,13 +28,13 @@ print("Pizza: ",32.0+68.0-14.0+82.0%4.0)
 # im afraid its now time to say goodbye be safe you kind gentleman and give this writer an A :)
 
 # Variables and some of their powers
-cars = 100
-space_in_a_car = 4.0
-drivers = 30
-passengers = 90
+cars = 80
+spaceInACar = 4.0
+drivers = 45
+passengers = 115
 cars_not_driven = cars - drivers
 car_driven = drivers
-carpool_capacity = space_in_a_car * cars_not_driven
+carpool_capacity = spaceInACar * cars_not_driven
 average_passengers_per_car = passengers / cars_not_driven
 
 print("There are" , cars, "cars available.")
@@ -42,4 +42,19 @@ print("There are only" , drivers, "drivers available today")
 print("There will be" , cars_not_driven, "empty cars today")
 print("We can transport" , carpool_capacity, "people today")
 print("We have" , passengers, "to carpool today")
-print("We need to put approximently" , average_passengers_per_car, "in each car")
+print("We need to put approximately" , average_passengers_per_car, "in each car")
+
+dough = 90
+size_of_dough = 16
+pizzamakers = 4
+customers = 35
+pizza_not_made = dough - pizzamakers
+pizza_made = pizzamakers
+average_pizzamaker_per_dough = pizzamakers / dough
+
+print("There are" , dough, "dough available.")
+print("Where are only" ,pizzamakers, "pizzamakers here today.")
+print("We have" , customers, "customers to make pizza for.")
+print("We need approximatley" , average_pizzamaker_per_dough, " dough for each customer.")
+
+
